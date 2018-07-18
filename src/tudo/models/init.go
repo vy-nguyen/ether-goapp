75e7c235cb11c14aaa7c15c7969689c3b0219194
Vy Nguyen <vy.n.nguyen@oracle.com>
2018-07-17 22:16:24 -0700

    Remove setup for extra database.

--- src/tudo/models/init.go
+++ src/tudo/models/init.go
@@ -17,16 +17,8 @@ func init() {
 		"@tcp(", conf.String("mysqlurls"), ":3306)/",
 		conf.String("mysqldb"), "?charset=utf8",
 	}
-	admin := []string{
-		conf.String("mysqluser"), ":", conf.String("mysqlpass"),
-		"@tcp(", conf.String("mysqlurls"), ":3306)/",
-		conf.String("admindb"), "?charset=utf8",
-	}
 	orm.RegisterDataBase("default", "mysql", strings.Join(part, ""))
-	orm.RegisterDataBase("admin", "mysql", strings.Join(admin, ""))
-
 	orm.RegisterModel(new(Account), new(Transaction), new(AccountKey))
 
 	orm.RunSyncdb("default", false, true)
-	orm.RunSyncdb("admin", false, true)
 }
